bdefca72c79d582d9a6c92c1abdeb9433cd951dd
ge jianyu <ge jianyu@detecon.com>
2018-01-26 19:09:36 +0800

    全部内容改为本地数据

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,7 +6,7 @@ import urllib.request
 import urllib.parse
 import urllib
 from templates.dashboard.Connect_DB \
-    import getCarOwner, getColumnChart_p1, getLevel1Attributes, getLevel2Attributes, getPurpose, people_get_pie, people_get_path, CP_get_cluster, Config_get_config, Config_get_config_local
+    import getCarOwner, getColumnChart_p1, getLevel1Attributes, getLevel2Attributes, getPurpose, people_get_pie, people_get_path, CP_get_cluster, Config_get_config, Config_get_config_local, Config_get_company, Config_get_model
 import json
 from django import forms
 from django.shortcuts import render,render_to_response
@@ -158,25 +158,30 @@ def CPChart(request):
     dict_ = {'cluster': data_cluster}
     return HttpResponse(json.dumps(dict_), content_type='application/json')
 
+
+
 # get Config_company
 # @login_required
 def ConfigCompany(request):
     id_ = request.GET.get('id', '')
     dict_city =[{"companyId":188,"companyName":"D-大众(进口)","seriesList":[{"seriesId":110,"seriesName":"辉腾","seriesEnglish":"phaeton","seriesLetter":"H"},{"seriesId":111,"seriesName":"途锐","seriesEnglish":"touareg","seriesLetter":"T"},{"seriesId":115,"seriesName":"甲壳虫","seriesEnglish":"beetle","seriesLetter":"J"},{"seriesId":116,"seriesName":"大众Eos","seriesEnglish":"eos","seriesLetter":"D"},{"seriesId":1110,"seriesName":"尚酷","seriesEnglish":"scirocco","seriesLetter":"S"},{"seriesId":1621,"seriesName":"Tiguan(进口)","seriesEnglish":"tiguan","seriesLetter":"T"},{"seriesId":2327,"seriesName":"迈特威","seriesEnglish":"multivan","seriesLetter":"M"},{"seriesId":3704,"seriesName":"PASSAT 进口","seriesEnglish":"vw-passat","seriesLetter":"P"},{"seriesId":4304,"seriesName":"大众CC 进口","seriesEnglish":"vwcc-1","seriesLetter":"D"},{"seriesId":4305,"seriesName":"迈腾(进口)","seriesEnglish":"magotan-1","seriesLetter":"M"},{"seriesId":5325,"seriesName":"高尔夫(进口)","seriesEnglish":"golf-1","seriesLetter":"G"},{"seriesId":5326,"seriesName":"夏朗","seriesEnglish":"xailang","seriesLetter":"X"},{"seriesId":103800,"seriesName":"凯路威","seriesEnglish":"caravelle","seriesLetter":"K"},{"seriesId":107220,"seriesName":"蔚揽","seriesEnglish":"weilan","seriesLetter":"W"}]},{"companyId":187,"companyName":"S-上汽大众","seriesList":[{"seriesId":104,"seriesName":"桑塔纳","seriesEnglish":"santana","seriesLetter":"S"},{"seriesId":105,"seriesName":"桑塔纳志俊","seriesEnglish":"vista","seriesLetter":"S"},{"seriesId":112,"seriesName":"途安","seriesEnglish":"touran","seriesLetter":"T"},{"seriesId":113,"seriesName":"朗逸","seriesEnglish":"lavida","seriesLetter":"L"},{"seriesId":2359,"seriesName":"PASSAT新领驭","seriesEnglish":"newpassat","seriesLetter":"P"},{"seriesId":2824,"seriesName":"途观L","seriesEnglish":"csvw-tiguan","seriesLetter":"T"},{"seriesId":3244,"seriesName":"POLO","seriesEnglish":"polo","seriesLetter":"P"},{"seriesId":5344,"seriesName":"帕萨特","seriesEnglish":"passat-1","seriesLetter":"P"},{"seriesId":5345,"seriesName":"高尔","seriesEnglish":"gol-1","seriesLetter":"G"},{"seriesId":101626,"seriesName":"朗行","seriesEnglish":"granlavida","seriesLetter":"L"},{"seriesId":102402,"seriesName":"朗境","seriesEnglish":"crosslavida","seriesLetter":"L"},{"seriesId":102620,"seriesName":"桑塔纳经典","seriesEnglish":"santana-classical","seriesLetter":"S"},{"seriesId":104763,"seriesName":"凌渡","seriesEnglish":"lamando","seriesLetter":"L"},{"seriesId":106020,"seriesName":"桑塔纳·浩纳","seriesEnglish":"gransantana","seriesLetter":"S"},{"seriesId":107382,"seriesName":"辉昂","seriesEnglish":"phideon","seriesLetter":"H"},{"seriesId":108501,"seriesName":"途昂","seriesEnglish":"g-suv","seriesLetter":"T"},{"seriesId":109060,"seriesName":"途观","seriesEnglish":"tuguan","seriesLetter":"T"}]},{"companyId":186,"companyName":"Y-一汽-大众","seriesList":[{"seriesId":103,"seriesName":"捷达","seriesEnglish":"jetta","seriesLetter":"J"},{"seriesId":107,"seriesName":"高尔夫","seriesEnglish":"golf","seriesLetter":"G"},{"seriesId":108,"seriesName":"速腾","seriesEnglish":"sagitar","seriesLetter":"S"},{"seriesId":1109,"seriesName":"CC","seriesEnglish":"vwcc","seriesLetter":"C"},{"seriesId":2325,"seriesName":"宝来","seriesEnglish":"bora","seriesLetter":"B"},{"seriesId":2348,"seriesName":"宝来经典","seriesEnglish":"bora-tc","seriesLetter":"B"},{"seriesId":5310,"seriesName":"开迪","seriesEnglish":"kaidi-1","seriesLetter":"K"},{"seriesId":6708,"seriesName":"迈腾","seriesEnglish":"b7l","seriesLetter":"M"},{"seriesId":107580,"seriesName":"高尔夫 嘉旅","seriesEnglish":"gaoerfujialv","seriesLetter":"G"},{"seriesId":108120,"seriesName":"蔚领","seriesEnglish":"c-trek","seriesLetter":"W"}]}]
-    url_pos = "http://auto.chexun.com/public/companySeriesJsonForPara/load.do?id={0}".format(id_)
-    result_pos = urllib.request.urlopen(url_pos)  # POST method
-    content_pos = result_pos.read().strip().decode('utf-8')
-    content_pos = eval(content_pos)
-    return HttpResponse(json.dumps(content_pos))
+    # url_pos = "http://auto.chexun.com/public/companySeriesJsonForPara/load.do?id={0}".format(id_)
+    # result_pos = urllib.request.urlopen(url_pos)  # POST method
+    # content_pos = result_pos.read().strip().decode('utf-8')
+    # content_pos = eval(content_pos)
+    # return HttpResponse(json.dumps(content_pos))
+    pos = Config_get_company(id_)
+    return HttpResponse(json.dumps(pos))
 
 # get Config_series
 # @login_required
 def ConfigSeriesChart(request):
     id_ = request.GET.get('id', '')
-    url_pos = "http://auto.chexun.com/public/yearModelJsonForPara/load.do?id={0}".format(id_)
-    result_pos = urllib.request.urlopen(url_pos)  # POST method
-    content_pos = result_pos.read().strip().decode('utf-8')
-    content_pos = eval(content_pos)
+    # url_pos = "http://auto.chexun.com/public/yearModelJsonForPara/load.do?id={0}".format(id_)
+    # result_pos = urllib.request.urlopen(url_pos)  # POST method
+    # content_pos = result_pos.read().strip().decode('utf-8')
+    # content_pos = eval(content_pos)
+    content_pos = Config_get_model(id_)
     return HttpResponse(json.dumps(content_pos))
 
 # get Config_modle
